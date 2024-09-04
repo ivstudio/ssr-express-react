49a7ef612a2e1181b792461e63c50a0e360057fc
Iggy Villamar <iggy@ivstudio.com>
2024-09-04 15:15:51 -0400

    add the BrowserRouter provider to testing utils

--- src/tests/customRender.tsx
+++ src/tests/customRender.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 export * from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+    return <BrowserRouter>{children}</BrowserRouter>;
+};
+
 const customRender = (ui: ReactElement, options = {}) => {
-    return render(ui, { ...options });
+    return render(ui, { wrapper: AllTheProviders, ...options });
 };
 
 export { customRender as render, userEvent };
