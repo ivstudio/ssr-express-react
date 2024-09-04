2db44fdfb965f3f50e3ba64585bfbe72fee32652
Iggy Villamar <iggy@ivstudio.com>
2024-09-04 15:21:19 -0400

    add Layout Outlet component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+import AppBar from '@/components/AppBar';
+
+const Layout = () => {
+    return (
+        <>
+            <AppBar />
+            <Outlet />
+        </>
+    );
+};
+
+export default Layout;
