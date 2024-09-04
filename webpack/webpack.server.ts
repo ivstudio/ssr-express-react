5c31c3a551989ca2a06f8e8c39fa2e0d774701ec
Iggy Villamar <iggy@ivstudio.com>
2024-09-04 15:13:34 -0400

    add webpack for ssr

---
+++ webpack/webpack.server.ts
@@ -0,0 +1,78 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import NodemonPlugin from 'nodemon-webpack-plugin';
+import path from 'path';
+import nodeExternals from 'webpack-node-externals';
+
+const serverConfig = {
+    name: 'server',
+    target: 'node',
+    mode: 'none',
+    externals: [nodeExternals()],
+    entry: path.resolve(__dirname, '../src/server/entry.server.ts'),
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, '../dist/server'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            '@': path.resolve(__dirname, '../src'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.module\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName:
+                                    '[name]__[local]-[hash:base64:5]',
+                            },
+                        },
+                    },
+                    'postcss-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|webp)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/[path][name].[hash:base64:5].[ext]',
+                            context: 'src',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash].css',
+            chunkFilename: 'css/[id].css',
+            ignoreOrder: false,
+        }),
+        new NodemonPlugin(),
+    ],
+};
+
+export default serverConfig;
